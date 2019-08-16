b7bf5cfe2c8e347b684d229325e00dc2b2d09e9f
Deepak Singh <deeputhakurgbn@gmail.com>
2019-08-17 00:07:20 +0530

    Comment removed

--- validation/register.js
+++ validation/register.js
@@ -3,20 +3,20 @@ const isEmpty = require("is-empty");
 
 module.exports = function validateRegisterInput(data) {
   let errors = {};
-// Convert empty fields to an empty string so we can use validator functions
+
   data.name = !isEmpty(data.name) ? data.name : "";
   data.roll = !isEmpty(data.roll) ? data.roll : "";
   data.password = !isEmpty(data.password) ? data.password : "";
   data.password2 = !isEmpty(data.password2) ? data.password2 : "";
-// Name checks
+
   if (Validator.isEmpty(data.name)) {
     errors.name = "Name field is required";
   }
-// roll checks
+
   if (Validator.isEmpty(data.roll)) {
     errors.roll = "roll field is required";
   } 
-// Password checks
+
   if (Validator.isEmpty(data.password)) {
     errors.password = "Password field is required";
   }
